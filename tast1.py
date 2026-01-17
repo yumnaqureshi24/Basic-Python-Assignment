181276a90299b05bdbe50120e34c35898ac7931f
yumnaqureshi24 <qureshiyumna24@gmail.com>
2026-01-17 15:30:14 +0530

    Add files via upload

---
+++ tast1.py
@@ -0,0 +1,7 @@
+#perform bacic mathematical operation
+a= float(input("enter first number :"))
+b= float(input("enter second number :"))
+print("addition =" , a+b)
+print("subtraction =" , a-b)
+print("multiplication =" , a*b)
+print("division =" , a/b)
---
+++ tast2.py
@@ -0,0 +1,7 @@
+#create a personalized greeting
+first_name = input ("enter your first name :")
+last_name = input ("enter your last name :")
+full_name = first_name + " " + last_name
+print("hello," , full_name + " ! welcome to python program .")
+
+
